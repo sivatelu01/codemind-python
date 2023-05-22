3ee719aa7de7765da614ad89fc6545c68ac82231
sivadarling01 <98811557+sivadarling01@users.noreply.github.com>
2023-05-22 15:09:09 +0530

    n=int(input()) for i in range(n):     a=""     for j in range(n,0,-1):         a=a+str(j)+" "     print(a)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,8 @@
 n=int(input())
-a=""
-for i in range(1,n+1):
-    a=a+str(chr(64+i))+" "
-    print(a*n)
-    a=
+for i in range(n):
+    a=""
+    for j in range(n,0,-1):
+        a=a+str(j)+" "
+    print(a)
+    
+    
