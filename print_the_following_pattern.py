6cde99803edb6d36402e112932ab244526a608f4
sivadarling01 <98811557+sivadarling01@users.noreply.github.com>
2023-05-22 15:30:35 +0530

    n=int(input()) a="x"*n for i in range(1,n+1):     print(a[:i-1]+"0"+a[i:])

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,4 @@
 n=int(input())
-for i in range(n):
-    a=""
-    for j in range(n,0,-1):
-        a=a+str(j)+" "
-    print(a)
-    
-    
+a="x"*n
+for i in range(1,n+1):
+    print(a[:i-1]+"0"+a[i:])
